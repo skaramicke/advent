cb0c74f562b62b5f8867b1f3f39bb233a9dadddc
Mikael Grön <skaramicke@gmail.com>
2022-12-06 07:05:29 +0100

    Make dec 6 -22 neater looking

--- day20221206/day20221206.go
+++ day20221206/day20221206.go
@@ -8,8 +8,8 @@ import (
 
 func Run() {
 	input := utils.ReadOrCreateInputFile("2022-12-06")
-	fmt.Printf("Number of chars: %d\n", find_marker_position(input, 4))
-	fmt.Printf("Number of chars: %d\n", find_marker_position(input, 14))
+	fmt.Printf("start-of-marker position:  %d\n", find_marker_position(input, 4))
+	fmt.Printf("start-of-message position: %d\n", find_marker_position(input, 14))
 }
 
 func find_marker_position(input string, marker_length int) int {
@@ -20,19 +20,17 @@ func find_marker_position(input string, marker_length int) int {
 		// add to most_recent_chars
 		most_recent_chars = append(most_recent_chars, char)
 
-		// Make sure it's 4 chars
+		// Make sure it's marker_length number of chars
 		if len(most_recent_chars) > marker_length {
 			most_recent_chars = most_recent_chars[1:]
-		}
 
-		if len(most_recent_chars) == marker_length {
 			// count how many of each char
 			char_counts := map[rune]int{}
 			for _, char := range most_recent_chars {
 				char_counts[char]++
 			}
-			fmt.Printf("%d: %c: %d, %c: %d, %c: %d, %c: %d\n", index, most_recent_chars[0], char_counts[most_recent_chars[0]], most_recent_chars[1], char_counts[most_recent_chars[1]], most_recent_chars[2], char_counts[most_recent_chars[2]], most_recent_chars[3], char_counts[most_recent_chars[3]])
-			// if all four counts are 1, return the index
+
+			// if all counts are 1, return the index
 			non_four := false
 			for _, char := range most_recent_chars {
 				if char_counts[char] != 1 {
@@ -43,6 +41,7 @@ func find_marker_position(input string, marker_length int) int {
 				return index + 1
 			}
 		}
+
 	}
 
 	return -1
